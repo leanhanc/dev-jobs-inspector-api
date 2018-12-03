ea9d26c40cdeb1bb2a393ae252f8c8d005600caf
leo10099 <hancleandro@gmail.com>
2018-12-03 20:48:34 -0300

    run cron every 5 hours

--- services/config.js
+++ services/config.js
@@ -46,7 +46,7 @@ const puppeteer = require('puppeteer');
 exports.puppetLaunch = async () => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: process.env.NODE_ENV === 'production' ? true : false,
+    //headless: process.env.NODE_ENV === 'production' ? true : false,
     defaultViewport: { width: 1200, height: 720 }
   });
 
